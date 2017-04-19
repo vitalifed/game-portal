d7a56f81b6bff16e934931233a0c41fa0727e7e4
Vitali Fedasenka <vitali.fedosenko@gmail.com>
2017-04-19 18:47:54 +0200

    Change name of the packages

--- mancala-game/src/test/java/org/bol/game/portal/mancala/intg/builder/Operation.java
+++
@@ -1,6 +0,0 @@
-package org.bol.game.portal.mancala.intg.builder;
-
-@FunctionalInterface
-public interface Operation<A, B, C, D, R> {
-	 public R apply (A a, B b, C c, D d) throws Exception;
-}
--- mancala-game/src/test/java/org/bol/game/portal/mancala/intg/builder/Validation.java
+++
@@ -1,6 +0,0 @@
-package org.bol.game.portal.mancala.intg.builder;
-
-@FunctionalInterface
-public interface Validation<A, B, R> {
-	 public R apply (A a, B b) throws Exception;
-}
